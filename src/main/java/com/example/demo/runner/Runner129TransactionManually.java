2d017495c04fe29e3b0a77092be0f42047aebb32
jiehuaou <jiehua.ou@gmail.com>
2023-05-12 14:39:06 +0800

    Programmatic Transaction

--- src/main/java/com/example/demo/runner/Runner129TransactionManually.java
+++ src/main/java/com/example/demo/runner/Runner129TransactionManually.java
@@ -2,16 +2,12 @@ package com.example.demo.runner;
 
 import com.example.demo.mapper.DepartmentRepository;
 import com.example.demo.model.Department;
-import com.example.demo.service.DepartmentService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -39,6 +35,12 @@ public class Runner129TransactionManually implements CommandLineRunner {
         log.info("-------------- testFailTransactionManually() --------------");
         testFailTransactionManually();
 
+        log.info("-------------- testFailTransactionTemplateManually() --------------");
+        testFailTransactionTemplateManually();
+
+        log.info("-------------- testSuccessTransactionTemplateManually() --------------");
+        testSuccessTransactionTemplateManually();
+
         log.info("-------------- findAll() --------------");
         departmentRepository.findAll().forEach(e -> log.info("{}", e.toString()));
     }
@@ -47,30 +49,62 @@ public class Runner129TransactionManually implements CommandLineRunner {
     void testSuccessTransactionManually() {
         TransactionStatus txStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
-            departmentRepository.create(Department.builder().id(1501L).departName("a005-Manual-Transaction-OK").leader("b005").build());
-            departmentRepository.create(Department.builder().id(1502L).departName("a005-Manual-Transaction-OK").leader("b005").build());
+            departmentRepository.create(Department.builder().id(1501L).departName("a005-Manual-Trans-OK").leader("b005").build());
+            departmentRepository.create(Department.builder().id(1502L).departName("a005-Manual-Trans-OK").leader("b005").build());
+            transactionManager.commit(txStatus);
         } catch (Exception e) {
             transactionManager.rollback(txStatus);
             log.info("TransactionException ==> {}", e);
         }
-        if(!txStatus.isCompleted()) {
-            transactionManager.commit(txStatus);
-        }
-    }
 
+    }
 
     void testFailTransactionManually() {
         TransactionStatus txStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
-            departmentRepository.create(Department.builder().id(1601L).departName("a005-Manual-Transaction-abort").leader("b005").build());
-            departmentRepository.create(Department.builder().id(1602L).departName("a005-Manual-Transaction-abort").leader("b005").build());
-            throw new RuntimeException("abort !!!");
+            departmentRepository.create(Department.builder().id(1601L).departName("a005-Manual-Trans-abort").leader("b005").build());
+            departmentRepository.create(Department.builder().id(1602L).departName("a005-Manual-Trans-abort").leader("b005").build());
+            abort();
+            transactionManager.commit(txStatus);
         } catch (Exception e) {
             transactionManager.rollback(txStatus);
-            log.info("TransactionException ==> {}", e);
+            log.info("TransactionException ==> {}", e.toString());
         }
-        if(!txStatus.isCompleted()) {
-            transactionManager.commit(txStatus);
-        }
+    }
+
+    void testFailTransactionTemplateManually() {
+
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.execute(status -> {
+            try {
+                departmentRepository.create(Department.builder().id(1701L).departName("a005-Manual-Trans-abort").leader("b005").build());
+                departmentRepository.create(Department.builder().id(1702L).departName("a005-Manual-Trans-abort").leader("b005").build());
+                abort();
+            } catch (Exception e) {
+                status.setRollbackOnly();
+                log.info("TransactionException ==> {}", e.toString());
+            }
+            return null;
+        });
+    }
+
+    void testSuccessTransactionTemplateManually() {
+
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.execute(status -> {
+            try {
+                departmentRepository.create(Department.builder().id(1801L).departName("a005-Manual-Trans-OK").leader("b005").build());
+                departmentRepository.create(Department.builder().id(1802L).departName("a005-Manual-Trans-OK").leader("b005").build());
+                //abort();
+            } catch (Exception e) {
+                status.setRollbackOnly();
+                log.info("TransactionException ==> {}", e.toString());
+            }
+            return null;
+        });
+    }
+
+    void abort() {
+        throw new RuntimeException("abort !!!");
     }
 }
